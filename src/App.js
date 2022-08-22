56c741495fb6b40416fc7ba51ee72a7a5302f155
shpilson <kovalenko.jora@gmail.com>
2022-08-22 12:52:21 +0300

    add styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import HomepageBanner from "./components/HomepageBanner/HomepageBanner";
+
 function App() {
   return (
     <div className="App">
       <h1>Hello World!</h1>
+      <HomepageBanner />
     </div>
   );
 }
