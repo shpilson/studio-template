e7a5afecb0ec822b6d5f1d757cbd3d200a6a99a2
shpilson <kovalenko.jora@gmail.com>
2022-08-26 03:17:14 +0300

    styles Homepage Banner Component & Button

--- src/components/HomepageBanner/HomepageBannerItem.js
+++ src/components/HomepageBanner/HomepageBannerItem.js
@@ -1,12 +1,41 @@
 import React from 'react'
 
 const HomepageBannerItem = (props) => {
-    const { id, bannerTitle, bannerDescription, bannerBg } = props
+    const { bannerTitle, bannerDescription, bannerBg } = props
     return (
         <div className='slideWrap' style={{ backgroundImage: `url(${bannerBg})` }}>
-            <div className='textWrap'>
-                <h2>{bannerTitle}</h2>
-                <p>{bannerDescription}</p>
+            <div className='container'>
+                <div className='textWrap'>
+                    <h2>{bannerTitle}</h2>
+                    <div className='line'></div>
+                    <p>{bannerDescription}</p>
+
+                    <svg xmlns="http://www.w3.org/2000/svg" version="1.1">
+                        <defs>
+                            <filter id="gooey">
+                                <feGaussianBlur in="SourceGraphic" stdDeviation="5" result="blur" />
+                                <feColorMatrix in="blur" type="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -9" result="highContrastGraphic" />
+                                <feComposite in="SourceGraphic" in2="highContrastGraphic" operator="atop" />
+                            </filter>
+                        </defs>
+                    </svg>
+
+                    <button id="gooey-button">
+                        Заказать разработку
+                        <span class="bubbles">
+                            <span class="bubble"></span>
+                            <span class="bubble"></span>
+                            <span class="bubble"></span>
+                            <span class="bubble"></span>
+                            <span class="bubble"></span>
+                            <span class="bubble"></span>
+                            <span class="bubble"></span>
+                            <span class="bubble"></span>
+                            <span class="bubble"></span>
+                            <span class="bubble"></span>
+                        </span>
+                    </button>
+                </div>
             </div>
         </div>
     )
