f594893d27eeef304cbb110bd4935c1b405c521c
shpilson <kovalenko.jora@gmail.com>
2022-08-29 17:23:32 +0300

    Header Navigation > add Language selector & phone button

--- src/components/HeaderNavigation/HeaderNavigationItem.js
+++ src/components/HeaderNavigation/HeaderNavigationItem.js
@@ -6,8 +6,24 @@ const HeaderNavigationItem = (props) => {
         <div className='container'>
             <div className='navbox'>
                 <a href="/">{studioName}</a>
-                <div className="phone">
-                    <span className={studioPhone}></span>
+
+                <div className='navbox-item'>
+                    <div className='phone'>
+                        <span className={studioPhone}></span>
+                    </div>
+
+                    <div className='crossline'></div>
+
+
+                    <select className='language'>
+                        <option>RU</option>
+                        <option>EN</option>
+                    </select>
+
+                </div>
+
+                <div className='menu'>
+
                 </div>
             </div>
         </div >
